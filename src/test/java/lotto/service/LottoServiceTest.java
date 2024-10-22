f0ac9b1a66fc55c17651439d3a5e0acc3a427c00
pum005 <pum005@naver.com>
2024-10-22 22:21:58 +0900

    refactor : 주석 제거

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,9 +1,6 @@
 package lotto.service;
 
-import lotto.model.Lotto;
-import lotto.model.Lottos;
-import lotto.model.Price;
-import lotto.model.Statistics;
+import lotto.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -22,15 +19,15 @@ public class LottoServiceTest {
         assertThat(lottos).isEqualTo(new Lottos(List.of(new Lotto(1,2,3,4,5,6), new Lotto(1,2,3,4,5,6))));
     }
 
-//    @Test
-//    void 통계() {
-//        LottoService lottoService = new LottoService(() -> List.of());
-//        Price price = new Price(2000);
-//        Lottos lottos = new Lottos(List.of(new Lotto(1, 2, 3, 7, 8, 9), new Lotto(1, 2, 3, 4, 7, 8)));
-//        Lotto winningLottos = new Lotto(1, 2, 3, 4, 5, 6);
-//
-//        Statistics statistics = lottoService.generateStatistics(lottos, winningLottos, price);
-//
-//        assertThat(statistics).isEqualTo(new Statistics(List.of(1L, 1L, 0L, 0L), 27.5));
-//    }
+    @Test
+    void 통계() {
+        LottoService lottoService = new LottoService(() -> List.of());
+        Price price = new Price(2000);
+        Lottos lottos = new Lottos(List.of(new Lotto(1, 2, 3, 7, 8, 9), new Lotto(1, 2, 3, 4, 7, 8)));
+        WinningLotto winningLottos = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        Statistics statistics = lottoService.generateStatistics(lottos, winningLottos, price);
+
+        assertThat(statistics).isEqualTo(new Statistics(List.of(1L, 1L, 0L, 0L, 0L), 27.5));
+    }
 }
